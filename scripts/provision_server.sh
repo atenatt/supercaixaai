5d435a7edcd888fb05a83a83299c4f82949f7bcc
vpovb <supercaixaai@gmail.com>
2024-09-14 15:51:59 -0300

    feat: Adicionado playbook para o Redis

--- scripts/provision_server.sh
+++ scripts/provision_server.sh
@@ -2,23 +2,18 @@
 # Atualizando pacotes e instalando Docker e Go no servidor
 apt-get update
 
-# Instalação do pacote Ansible
-apt-get install -y ansible
-
 # Instalação do Nginx
 apt-get install -y nginx
+
+# Instalando Go
+wget https://golang.org/dl/go1.17.6.linux-amd64.tar.gz
+tar -C /usr/local -xzf go1.17.6.linux-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin
+echo "export PATH=$PATH:/usr/local/go/bin" >> /etc/profile
 
 # Iniciando e habilitando o Nginx
 systemctl start nginx
 systemctl enable nginx
-
-# Adicionando os hosts no diretório /etc/hosts
-sudo bash -c "cat <<EOT >> /etc/hosts
-172.16.0.1 srv-sc01
-172.16.0.2 db-sc01
-172.16.0.11 pdv-sc01
-172.16.0.12 pdv-sc02
-EOT"
 
 # Removendo a chave ssh antiga
 sudo rm -rf /shared/ssh_key/id_rsa*
@@ -26,7 +21,7 @@ sudo rm -rf /shared/ssh_key/id_rsa*
 # Gerando a nova chave ssh
 ssh-keygen -t rsa -b 4096 -f /shared/ssh_key/id_rsa -N ""
 
-# Copiando a chave ssh gerada para o /home/vagrant/.ssh/id_rsa
+# Copiando a chave ssh gerada para o /home/vagrant/.ssh/
 cp /shared/ssh_key/id_rsa /home/vagrant/.ssh/
 
 # Adicionando os hosts no arquivo /etc/ansible/hosts
@@ -37,3 +32,12 @@ pdv-sc02
 [dbs]
 db-sc01
 EOT"
+
+# Copiando os arquivos index.html e style.css para o diretório do Nginx
+cp /shared/web/* /var/www/html/
+
+# Instalação do Ansible
+apt-get install -y ansible
+
+# Executar o playbook do Ansible para instalar e configurar Redis
+ansible-playbook /shared/scripts/ansible/install_redis.yml -i /shared/scripts/ansible/hosts.ini
