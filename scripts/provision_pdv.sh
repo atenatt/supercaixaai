e47a508383cabcfc466c05cbfd167307026c5f6a
vpovb <supercaixaai@gmail.com>
2024-09-14 15:50:29 -0300

    feat: Adiciona o executavel da interface e o alias

--- scripts/provision_pdv.sh
+++ scripts/provision_pdv.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 # Atualizando pacotes e instalando Docker, Go e Python nos PDVs
 apk update
-apk add python3 py3-pip
+apk add python3 py3-pip dialog redis
 
 # Criar link simbólico de python para python3
 ln -s /usr/bin/python3 /usr/bin/python
 
 # Adicionando a chave ssh do host na maquina
 cat /shared/ssh_key/id_rsa.pub >> /home/vagrant/.ssh/authorized_keys
+
+# Tornar o script da interface executável
+chmod +x /shared/scripts/pdv_interface.sh
+
+# Adicionar alias para facilitar o uso
+echo "alias pdv='/shared/scripts/pdv_interface.sh'" >> /home/vagrant/.profile
