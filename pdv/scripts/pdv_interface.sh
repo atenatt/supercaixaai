8690426813491b881696c4aa98a27fff44677152
atenatt <victor.boas@fatec.sp.gov.br>
2024-09-24 08:02:00 -0300

    feat: novo arquivo de interface

--- pdv/scripts/pdv_interface.sh
+++ pdv/scripts/pdv_interface.sh
@@ -1,53 +1,26 @@
 #!/bin/sh
 
-# Configurações
-DB_HOST="172.16.0.2"
+# Carregar o arquivo de funções
+source /etc/pdv/funcoes.sh
 
-# Função para cadastrar mercadoria
-cadastrar_mercadoria() {
-  NOME=$(dialog --stdout --inputbox "Nome da Mercadoria:" 0 0)
-  [ $? -ne 0 ] && return
-
-  PRECO_CUSTO=$(dialog --stdout --inputbox "Preço de Custo:" 0 0)
-  [ $? -ne 0 ] && return
-
-  PRECO_VENDA=$(dialog --stdout --inputbox "Preço de Venda:" 0 0)
-  [ $? -ne 0 ] && return
-
-  # Salvar no Redis
-  redis-cli -h $DB_HOST HMSET "mercadoria:$NOME" nome "$NOME" preco_custo "$PRECO_CUSTO" preco_venda "$PRECO_VENDA"
-
-  dialog --msgbox "Mercadoria cadastrada com sucesso!" 6 40
-}
-
-# Função para consultar mercadoria
-consultar_mercadoria() {
-  NOME=$(dialog --stdout --inputbox "Nome da Mercadoria:" 0 0)
-  [ $? -ne 0 ] && return
-
-  # Recuperar do Redis
-  RESULTADO=$(redis-cli -h $DB_HOST HGETALL "mercadoria:$NOME")
-
-  if [ -z "$RESULTADO" ]; then
-    dialog --msgbox "Mercadoria não encontrada." 6 40
-  else
-    dialog --msgbox "Dados da Mercadoria:\n$RESULTADO" 10 50
-  fi
-}
-
-# Loop principal
+# Loop principal do menu
 while true; do
-  OPCAO=$(dialog --stdout --menu "SuperCaixa AI - PDV" 0 0 0 \
+  OPCAO=$(dialog --stdout --menu "SuperCaixa AI - PDV" 15 50 4 \
     1 "Cadastrar Mercadoria" \
-    2 "Consultar Mercadoria" \
-    3 "Sair")
+    2 "Cadastrar Usuário" \
+    3 "Abrir Caixa" \
+    4 "Consultar Mercadoria" \
+    5 "Sair")
   
+  # Verifica se o usuário cancelou ou saiu do menu
   [ $? -ne 0 ] && break
 
   case $OPCAO in
     1) cadastrar_mercadoria ;;
-    2) consultar_mercadoria ;;
-    3) break ;;
+    2) cadastrar_usuario ;;
+    3) abrir_caixa ;;
+    4) consultar_mercadoria ;;
+    5) break ;;
   esac
 done
 
