41e4ec5283f43701e3b6c7325b9562aa81afd217
atenatt <victor.boas@fatec.sp.gov.br>
2024-09-24 11:58:24 -0300

    fix: alterando a autenticação para admin

--- pdv/scripts/funcs/consultar_mercadoria.sh
+++ pdv/scripts/funcs/consultar_mercadoria.sh
@@ -2,12 +2,16 @@
 
 # Função para consultar mercadoria (Fiscal e Admin)
 consultar_mercadoria() {
-  autenticar_usuario "fiscal" || return 1
+  # Verifica se o usuário é fiscal ou administrador
+  autenticar_usuario "fiscal" || autenticar_usuario "admin" || return 1
 
   CODIGO=$(dialog --stdout --inputbox "Código GTIN ou Interno:" 0 0)
   [ $? -ne 0 ] && return
 
+  # Buscar dados da mercadoria no Redis
   RESULTADO=$(redis-cli -h $DB_HOST HGETALL "mercadoria:$CODIGO")
+
+  # Verificar se a mercadoria foi encontrada
   if [ -z "$RESULTADO" ]; then
     dialog --msgbox "Mercadoria não encontrada." 6 40
   else
