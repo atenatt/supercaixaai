77683aab708a29133e095ba752e2a21a68e6e868
atenatt <victor.boas@fatec.sp.gov.br>
2024-09-24 14:06:08 -0300

    fix: alterando layout da consulta

--- pdv/scripts/funcs/consultar_mercadoria.sh
+++ pdv/scripts/funcs/consultar_mercadoria.sh
@@ -1,19 +1,23 @@
 #!/bin/sh
 
-# Função para consultar mercadoria específica (Fiscal e Admin)
+# Função para consultar mercadoria (Fiscal e Admin)
 consultar_mercadoria() {
+  # Verifica se o usuário é fiscal ou administrador
   autenticar_usuario "fiscal" || autenticar_usuario "admin" || return 1
 
   CODIGO=$(dialog --stdout --inputbox "Código GTIN ou Interno:" 0 0)
   [ $? -ne 0 ] && return
 
-  NOME=$(redis-cli -h $DB_HOST HGET "mercadoria:$CODIGO" nome)
-  PRECO_VENDA=$(redis-cli -h $DB_HOST HGET "mercadoria:$CODIGO" preco_venda)
-  ESTOQUE=$(redis-cli -h $DB_HOST HGET "mercadoria:$CODIGO" estoque)
+  # Buscar dados da mercadoria no Redis
+  RESULTADO=$(redis-cli -h $DB_HOST HGETALL "mercadoria:$CODIGO")
 
-  if [ -z "$NOME" ] || [ -z "$PRECO_VENDA" ] || [ -z "$ESTOQUE" ]; then
-    dialog --msgbox "Mercadoria não encontrada!" 6 40
+  if [ -z "$RESULTADO" ]; then
+    dialog --msgbox "Mercadoria não encontrada." 6 40
   else
-    dialog --msgbox "Código: $CODIGO\nNome: $NOME\nPreço de Venda: R$ $PRECO_VENDA\nEstoque: $ESTOQUE" 10 50
+    NOME=$(redis-cli -h $DB_HOST HGET "mercadoria:$CODIGO" nome)
+    PRECO_VENDA=$(redis-cli -h $DB_HOST HGET "mercadoria:$CODIGO" preco_venda)
+    ESTOQUE=$(redis-cli -h $DB_HOST HGET "mercadoria:$CODIGO" estoque)
+    SETOR=$(redis-cli -h $DB_HOST HGET "mercadoria:$CODIGO" setor)
+    dialog --msgbox "Nome: $NOME\nPreço de Venda: R$ $PRECO_VENDA\nEstoque: $ESTOQUE\nSetor: $SETOR" 10 50
   fi
 }
