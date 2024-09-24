3432fb0c2f4cb6228f2b28f535dc7f10c7a07fa2
atenatt <victor.boas@fatec.sp.gov.br>
2024-09-24 11:58:48 -0300

    fix: alterando a autenticacao

--- pdv/scripts/funcs/autenticar_usuario.sh
+++ pdv/scripts/funcs/autenticar_usuario.sh
@@ -1,27 +1,35 @@
 #!/bin/sh
 
-# Função para autenticar usuário
+# Função para autenticar o usuário
 autenticar_usuario() {
-  ROLE_REQUERIDA=$1
-  USUARIO=$(dialog --stdout --inputbox "Usuário:" 0 0)
+  ROLE_REQUERIDA="$1"
+
+  # Solicitar o nome do usuário e a senha
+  USUARIO=$(dialog --stdout --inputbox "Nome do Usuário:" 0 0)
   [ $? -ne 0 ] && return 1
 
   SENHA=$(dialog --stdout --passwordbox "Senha:" 0 0)
   [ $? -ne 0 ] && return 1
 
-  # Recuperar as informações do usuário no Redis
-  SENHA_SALVA=$(redis-cli -h $DB_HOST HGET "usuario:$USUARIO" senha)
+  # Buscar role e senha do usuário no Redis
   ROLE=$(redis-cli -h $DB_HOST HGET "usuario:$USUARIO" role)
+  SENHA_CORRETA=$(redis-cli -h $DB_HOST HGET "usuario:$USUARIO" senha)
 
-  if [ "$SENHA" = "$SENHA_SALVA" ]; then
-    if [ "$ROLE_REQUERIDA" = "$ROLE" ] || [ "$ROLE_REQUERIDA" = "admin" ]; then
-      return 0  # Autenticação bem-sucedida
-    else
-      dialog --msgbox "Acesso negado! Função requerida: $ROLE_REQUERIDA" 6 40
-      return 1
-    fi
-  else
+  # Verificar se a senha está correta
+  if [ "$SENHA" != "$SENHA_CORRETA" ]; then
     dialog --msgbox "Senha incorreta!" 6 40
-    return 1  # Falha na autenticação
+    return 1
   fi
+
+  # Verificar se a role do usuário corresponde à role requerida
+  if [ "$ROLE" != "$ROLE_REQUERIDA" ]; then
+    # Se for "admin", também deve ser permitido
+    if [ "$ROLE_REQUERIDA" = "fiscal" ] && [ "$ROLE" = "admin" ]; then
+      return 0
+    fi
+    dialog --msgbox "Acesso negado! Função requerida: $ROLE_REQUERIDA" 6 40
+    return 1
+  fi
+
+  return 0
 }
