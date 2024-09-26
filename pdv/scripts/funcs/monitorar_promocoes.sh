e2fe4dba016791871c51058b467161a687f76167
atenatt <victor.boas@fatec.sp.gov.br>
2024-09-26 10:35:38 -0300

    feat: script para monitorar as promocoes ativas

---
+++ pdv/scripts/funcs/monitorar_promocoes.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+# Função para monitorar promoções e restaurar preços
+monitorar_promocoes() {
+  while true; do
+    TEMPO_ATUAL=$(date +%s)
+
+    # Listar todas as promoções ativas
+    PROMOCOES=$(redis-cli -h $DB_HOST KEYS "promocao:*")
+
+    for PROMOCAO in $PROMOCOES; do
+      CODIGO=$(echo $PROMOCAO | cut -d':' -f2)
+      DATA_EXPIRACAO=$(redis-cli -h $DB_HOST HGET "promocao:$CODIGO" expira_em)
+
+      # Verificar se a promoção expirou
+      if [ "$TEMPO_ATUAL" -ge "$DATA_EXPIRACAO" ]; then
+        # Restaurar o preço original
+        PRECO_ORIGINAL=$(redis-cli -h $DB_HOST HGET "mercadoria:$CODIGO" preco_original)
+        redis-cli -h $DB_HOST HSET "mercadoria:$CODIGO" preco_venda "$PRECO_ORIGINAL"
+
+        # Remover a promoção
+        redis-cli -h $DB_HOST DEL "promocao:$CODIGO"
+        redis-cli -h $DB_HOST HDEL "mercadoria:$CODIGO" preco_original
+
+        # Registrar log da expiração
+        registrar_log "admin" "Promoção expirada" "Produto: $CODIGO, Preço restaurado para $PRECO_ORIGINAL"
+      fi
+    done
+
+    # Verificar promoções a cada 1 hora
+    sleep 3600
+  done
+}
+
+# Iniciar o monitoramento de promoções
+monitorar_promocoes
