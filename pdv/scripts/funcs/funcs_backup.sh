da02cd13779e3695541cf3fb8e7018b4303eca28
Victor Pedro <victor.boas@fatec.sp.gov.br>
2024-09-28 17:34:43 -0300

    feat: adicionando novas funcionalidades ao log de funcs

--- pdv/scripts/funcs/funcs_backup.sh
+++ pdv/scripts/funcs/funcs_backup.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 
-# Carregar a função de log
-source /etc/pdv/funcs/funcs_logs.sh
+# Definir o diretório base para os logs
+BASE_LOG_DIR="/var/log/funcoes"
 
-# Criar diretório de logs, caso não exista
-LOG_DIR="/var/log/funcoes"
+# Capturar a data e hora no formato desejado
+DATA=$(date '+%d-%m-%y')
+HORA=$(date '+%H:00')
+
+# Criar a pasta do dia, caso não exista
+LOG_DIR="$BASE_LOG_DIR/$DATA"
 mkdir -p "$LOG_DIR"
-LOG_FILE="$LOG_DIR/funcoes_$(date '+%Y-%m-%d_%H-%M').log"
+
+# Definir o nome do arquivo de log com base na hora
+LOG_FILE="$LOG_DIR/$HORA.log"
 
 # Capturar o nome do script e o usuário que executou
 SCRIPT_NAME=$(basename "$0")
@@ -15,7 +21,7 @@ USUARIO=$(whoami)
 # Função para logar ações no arquivo de log
 log() {
   local MENSAGEM="$1"
-  echo "[$(date '+%Y-%m-%d %H:%M:%S')] [Usuário: $USUARIO] [Script: $SCRIPT_NAME] $MENSAGEM" >> "$LOG_FILE"
+  echo "[$(date '+%d-%m-%y %H:%M:%S')] [Usuário: $USUARIO] [Script: $SCRIPT_NAME] $MENSAGEM" >> "$LOG_FILE"
 }
 
 # Função para realizar backup do banco de dados Redis
@@ -27,23 +33,27 @@ backup_banco() {
   BACKUP_FILE="$DATA_DIR/redis_backup_$(date '+%Y-%m-%d_%H-%M-%S').rdb"
 
   # Testar a conexão com o Redis
-  log "Conexão com o Redis - STATUS: SUCESSO"
+  log "Testando conexão com o Redis..."
   redis-cli -h redis_db ping
   if [ $? -ne 0 ]; then
     log "Erro ao conectar com o Redis."
     return 1
   fi
 
+  log "Conexão com o Redis - STATUS: SUCESSO"
+
   # Enviar o comando para o Redis para salvar o banco de dados
-  log "Envio do comando de backup ao Redis - STATUS: SUCESSO"
+  log "Enviando comando de backup ao Redis..."
   redis-cli -h redis_db SAVE
   if [ $? -ne 0 ]; then
     log "Erro ao salvar o banco de dados no Redis."
     return 1
   fi
 
+  log "Comando de backup enviado ao Redis - STATUS: SUCESSO"
+
   # Verificar se o dump.rdb foi gerado no Redis
-  log "Verificação de criação do arquivo dump.rdb - STATUS: SUCESSO"
+  log "Verificando se o arquivo dump.rdb foi criado..."
   if [ ! -f "$DATA_DIR/dump.rdb" ]; then
     log "Erro: Arquivo dump.rdb não encontrado no Redis."
     return 1
@@ -52,12 +62,14 @@ backup_banco() {
   fi
 
   # Copiar o arquivo dump.rdb para o diretório de backup
-  log "Cópia do arquivo dump.rdb para o diretório de backup - STATUS: SUCESSO"
+  log "Copiando o arquivo dump.rdb para o diretório de backup..."
   cp "$DATA_DIR/dump.rdb" "$BACKUP_FILE"
   if [ $? -ne 0 ]; then
     log "Erro ao copiar o arquivo de backup."
     return 1
   fi
+
+  log "Arquivo dump.rdb copiado com sucesso para o diretório de backup - STATUS: SUCESSO"
 
   # Registrar o log do backup realizado
   ACAO="Realizou backup do banco de dados"
