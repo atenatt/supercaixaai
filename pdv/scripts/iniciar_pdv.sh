6696195959844962a68b202f213c10c0c470ad82
atenatt <victor.boas@fatec.sp.gov.br>
2024-09-30 08:57:44 -0300

    fix: carregando todos scripts de funcs

--- pdv/scripts/iniciar_pdv.sh
+++ pdv/scripts/iniciar_pdv.sh
@@ -1,8 +1,21 @@
 #!/bin/sh
 
-# Carregar funções do PDV
-source /etc/pdv/funcs/funcs_venda.sh
-source /etc/pdv/funcs/funcs_logs.sh 
+# Carregar todas as funções a partir de /etc/pdv/funcs/
+echo "Carregando funções automaticamente..."
+for func_script in /etc/pdv/funcs/*.sh; do
+  if [ -f "$func_script" ]; then
+    # Verifica se o script é o monitorar_promocoes.sh e o ignora
+    if [ "$(basename "$func_script")" = "monitorar_promocoes.sh" ]; then
+      echo "Ignorando $func_script..."
+      continue
+    fi
+
+    echo "Carregando $func_script..."
+    source "$func_script" || { echo "Erro ao carregar $func_script"; exit 1; }
+  fi
+done
+
+echo "Todas as funções foram carregadas com sucesso."
 
 # Função principal para iniciar o PDV
 iniciar_pdv() {
@@ -18,7 +31,7 @@ iniciar_pdv() {
     [ $? -ne 0 ] && clear && exit 0
 
     case $OPCAO in
-      1) iniciar_pdv ;;       # Função de abrir o caixa
+      1) abrir_caixa ;;       # Função de abrir o caixa
       2) registrar_venda ;;   # Função de registrar vendas
       3) consultar_mercadoria ;; # Função para consultar mercadorias
       4) fechar_caixa ;;      # Função de fechar o caixa
