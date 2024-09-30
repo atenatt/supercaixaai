b29c0ef55b99cd63f2d656fc2de9291ab387e9c3
atenatt <victor.boas@fatec.sp.gov.br>
2024-09-30 08:18:07 -0300

    feat: adicionando logs as funcs de setor

--- pdv/scripts/funcs/funcs_setor.sh
+++ pdv/scripts/funcs/funcs_setor.sh
@@ -1,31 +1,35 @@
 #!/bin/sh
 
-# Carregar a função de log
 source /etc/pdv/funcs/funcs_logs.sh
 
 # Função para criar setor
 criar_setor() {
+  log_funcs "Iniciando criação de setor."
+
   # Verifica se o administrador já está autenticado
   if [ "$ADMIN_AUTENTICADO" -ne 1 ]; then
     autenticar_usuario "admin" || return 1
   fi
 
+  log_funcs "Usuário autenticado com sucesso para criação de setor."
+
   # Solicita o nome do setor
   NOME_SETOR=$(dialog --stdout --inputbox "Nome do Setor (ex: FRIOS, HORTIFRUTI):" 0 0)
-  [ $? -ne 0 ] && return
+  [ $? -ne 0 ] && log_funcs "Criação de setor cancelada pelo usuário." && return
 
   # Verifica se o setor já existe
   SETOR_EXISTENTE=$(redis-cli -h $DB_HOST EXISTS "setor:$NOME_SETOR")
   if [ "$SETOR_EXISTENTE" -eq 1 ]; then
     dialog --msgbox "Setor já cadastrado!" 6 40
+    log_funcs "Tentativa de criação falhou: Setor já cadastrado - Nome: $NOME_SETOR."
     return 1
   fi
 
   # Salva o setor no banco de dados
   redis-cli -h $DB_HOST SADD "setores" "$NOME_SETOR"
-
+  
   # Registrar log da ação de criação de setor
-  registrar_log "admin" "Criou setor" "Nome do Setor: $NOME_SETOR"
+  log_funcs "Criou setor: Nome do Setor: $NOME_SETOR"
 
   # Exibe mensagem de sucesso
   dialog --msgbox "Setor $NOME_SETOR criado com sucesso!" 6 40
@@ -33,16 +37,21 @@ criar_setor() {
 
 # Função para consultar mercadorias por setor
 consultar_por_setor() {
+  log_funcs "Iniciando consulta de mercadorias por setor."
+
   # Verifica se o administrador já está autenticado
   if [ "$ADMIN_AUTENTICADO" -ne 1 ]; then
     autenticar_usuario "admin" || return 1
   fi
+
+  log_funcs "Usuário autenticado com sucesso para consulta de mercadorias por setor."
 
   # Coletar os setores disponíveis
   SETORES=$(redis-cli -h $DB_HOST SMEMBERS "setores")
 
   if [ -z "$SETORES" ]; then
     dialog --msgbox "Nenhum setor cadastrado." 6 50
+    log_funcs "Nenhum setor cadastrado encontrado."
     return 1
   fi
 
@@ -54,7 +63,9 @@ consultar_por_setor() {
 
   # Selecionar o setor
   SETOR=$(dialog --stdout --menu "Selecione o setor para listar itens:" 15 50 6 $OPCOES_SETORES)
-  [ $? -ne 0 ] && return
+  [ $? -ne 0 ] && log_funcs "Consulta de mercadorias por setor cancelada pelo usuário." && return
+
+  log_funcs "Setor selecionado para consulta: $SETOR."
 
   # Buscar mercadorias do setor no Redis
   MERCADORIAS=$(redis-cli -h $DB_HOST KEYS "mercadoria:*")
@@ -73,7 +84,9 @@ consultar_por_setor() {
 
   if [ -z "$LISTAGEM" ]; then
     dialog --msgbox "Nenhuma mercadoria encontrada no setor $SETOR." 6 40
+    log_funcs "Nenhuma mercadoria encontrada no setor $SETOR."
   else
     dialog --msgbox "Mercadorias do setor $SETOR:\n$LISTAGEM" 20 60
+    log_funcs "Mercadorias do setor $SETOR consultadas com sucesso."
   fi
 }
